dec9551b77af8959e5df32208f13b0a1cc3b4d38
roen@roen-PC <roen@roen-PC>
2011-09-08 02:49:37 +0900

    주석제거

--- i4core_test/i4core_test.cpp
+++ i4core_test/i4core_test.cpp
@@ -12,62 +12,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	I4Log::initialize(I4Log::FLAG_CONSOLE|I4Log::FLAG_DEBUGGER|I4Log::FLAG_FILE, I4Log::LEVEL_DEBUG, L"i4.log");
 	I4StopWatch::initialize();
-	/*
-	I4StopWatch stopWatch;
 
-	stopWatch.reset();
-	for (int i = 0; i < 10; i++)
-	{
-		{
-			PROFILE("debug");
-			I4LOG_DEBUG << L"debug";
-		}
-
-		for (int j = 0; j < 10; j++)
-		{
-			PROFILE("info");
-			I4LOG_INFO << L"info";
-		}
-			
-		{
-			PROFILE("warning");
-			I4LOG_WARNING << L"warning";
-		}
-
-		for (int k = 0; k < 50; k++)
-		{
-			PROFILE("error");
-			I4LOG_ERROR << L"error";
-
-			int sum = 0;
-			for (int l = 0; l < 5; l++)
-			{
-				PROFILE("error_child");
-				sum += l;
-			}
-
-			I4LOG_ERROR << sum;
-		}
-		{
-			PROFILE("fatal");
-			I4LOG_FATAL << L"fatal";
-		}
-	}
-	
-	I4LOG_DEBUG << stopWatch.getElapsedTime();
-
-	I4ProfileIterator itr = I4ProfileManager::getRootIterator();
-
-	do
-	{
-		I4LOG_DEBUG << itr.getCurChildName() << L"\t" << itr.getCurChildTotalTime() << L"\t" << itr.getCurChildTotalCalls();
-		itr.next();
-	} while (!itr.isDone());
-
-	I4ProfileWriter::writeJson(I4ProfileManager::getRootNode(), L"profile.json");
-
-	I4Log::finalize();
-	*/
 	::testing::InitGoogleTest(&argc, argv);
 
 	return RUN_ALL_TESTS();
